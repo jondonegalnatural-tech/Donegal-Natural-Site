2d053d6e8f5b91bf6ba67768b2347d352e9d483c
Jonathan Ackerman <Jon.DonegalNatural@gmail.com>
2026-09-02 12:17:08 -0400

    Stop service worker from intercepting login and dashboard requests

--- sw.js
+++ sw.js
@@ -5,7 +5,3 @@ self.addEventListener('install', function (event) {
 self.addEventListener('activate', function (event) {
     event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', function (event) {
-    event.respondWith(fetch(event.request));
-});
