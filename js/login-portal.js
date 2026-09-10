4b331068069df713e2da86efa8df982d17937501
Jonathan Ackerman <Jon.DonegalNatural@gmail.com>
2026-09-10 09:15:05 -0400

    Update login portal and Vercel config

--- js/login-portal.js
+++ js/login-portal.js
@@ -322,3 +322,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(function () {});
+}
